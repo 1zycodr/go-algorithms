bee47e5318ce444dbb68114a117e5ee41eddd9c6
mukhitdinov.a <5zamiggos@gmail.com>
2023-12-01 17:44:33 +0600

    counting sort

---
+++ sorting/counting_sort.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"sort"
+	"time"
+)
+
+func countingSort(arr []int) []int { // O(n + m), m = max(arr)
+	max, min := math.MinInt32, math.MaxInt32
+	for _, v := range arr { // O(n)
+		if v > max {
+			max = v
+		}
+		if v < min {
+			min = v
+		}
+	}
+	temp := make([]int, max-min+1)
+	for _, v := range arr { // O(n)
+		temp[v-min]++
+	}
+	k := 0
+	for i, v := range temp { // O(m), m = max(arr)
+		for v != 0 {
+			v--
+			arr[k] = i + min
+			k++
+		}
+	}
+	return arr
+}
+
+func main() {
+	rand.NewSource(time.Now().Unix())
+	n := 100000
+
+	arr := make([]int, 0)
+	for i := 0; i < n; i++ {
+		arr = append(arr, rand.Intn(10000000)+1)
+	}
+
+	arrCopy := make([]int, n)
+	copy(arrCopy, arr)
+	start := time.Now()
+	sort.Ints(arrCopy)
+	fmt.Println("sort.Sort time:", time.Since(start))
+
+	start = time.Now()
+	arr = countingSort(arr)
+	fmt.Println("countingSort time:", time.Since(start))
+
+	for i := 0; i < n; i++ {
+		if arr[i] != arrCopy[i] {
+			fmt.Println("Error while testing sorting")
+			os.Exit(1)
+		}
+	}
+
+	fmt.Println("Success!")
+}
